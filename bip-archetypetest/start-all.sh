3fd1ea1b9d02a6a67bb21c697276c77e65525370
Freshwater <594058@bah.com>
2019-11-26 15:45:08 -0500

    Cleanup files from directory shift

--- bip-archetypetest/start-all.sh
+++
@@ -1,4 +0,0 @@
-# Starts the openshift archetypetest spring boot service, including consul, vault and redis
-
-docker-compose -f docker-compose.yml \
-	up --build -d
--- bip-archetypetest/stop-all.sh
+++
@@ -1,4 +0,0 @@
-# Stops the entire platform, including all log aggregation services
-
-docker-compose -f docker-compose.yml \
-	down -v
